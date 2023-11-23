073f46d88bd1ad2087b93c0f09b640e1cb9a9b1d
Lara-Jalloul <laraadjalloul@gmail.com>
2023-11-23 03:05:21 +0200

    navbar responsive in about page

---
+++ scroll.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function () {
+  //Get the button
+  let mybutton = document.getElementById("goTopBtn");
+
+  // When the user scrolls down 20px from the top of the document, show the button
+  window.onscroll = function () {
+    scrollFunction();
+  };
+
+  function scrollFunction() {
+    if (
+      document.body.scrollTop > 20 ||
+      document.documentElement.scrollTop > 20
+    ) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
+  }
+});
