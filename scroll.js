eb1255e10f0be53401d6f588c03ae86f856750db
Lara-Jalloul <laraadjalloul@gmail.com>
2023-11-28 14:07:36 +0200

    done

--- scroll.js
+++ scroll.js
@@ -1,17 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
-  //Get the button
   let mybutton = document.getElementById("goTopBtn");
 
-  // When the user scrolls down 20px from the top of the document, show the button
   window.onscroll = function () {
     scrollFunction();
   };
 
   function scrollFunction() {
-    if (
-      document.body.scrollTop > 20 ||
-      document.documentElement.scrollTop > 20
-    ) {
+    let scrollTop =
+      document.body.scrollTop || document.documentElement.scrollTop;
+    if (scrollTop > 20) {
       mybutton.style.display = "block";
     } else {
       mybutton.style.display = "none";
